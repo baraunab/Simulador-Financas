8599baf68568bd7f542e7169dd525d4e76928428
beatriz <bbarauna.beatriz@gmail.com>
2025-10-02 13:04:07 -0400

    Funcao para definir a data de vencimento

--- res/scr/formulario.js
+++ res/scr/formulario.js
@@ -1,3 +1,22 @@
 function validarNumero(input) {
   input.value = input.value.replace(/[^0-9]/g, '');
 }
+
+function definirDataVenc() {
+  //Pega data local e adiciona 180 dias
+  const dataAtual = new Date();
+  dataAtual.setDate(dataAtual.getDate() + 180); 
+  
+  //Formatação da data em dd/mm/yyyy
+  const dia = String(dataAtual.getDate()).padStart(2, '0');
+  const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+  const ano = dataAtual.getFullYear();
+
+  return `${ano}-${mes}-${dia}`;
+}
+    
+//Define a data de vencimento no input
+document.getElementById("opcoesInvestimento").addEventListener("change", function() {
+  const dataVencimento = definirDataVenc();
+  document.getElementById("dataV").value = dataVencimento;
+});
